21edfeff50548279d41c2440d8f06d3942e6b0d1
zahra <zahrafouladian@gmail.com>
2023-09-29 20:29:13 +0330

    git commit

--- Basic Python Project/Object-Oriented Programming Problem/shirtificate/shirtificate.py
+++
@@ -1,7 +0,0 @@
-from fpdf import FPDF
-def main():
-    name = input("Name:")
-    pdf = FPDF()
-    pdf.add_page(orientation="portrait", format="a4")
-    pdf.image("./shirtificate.png", x=20, y=60)
-    pdf.output("shirtificate.pdf")
